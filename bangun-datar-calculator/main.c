924bf181f842730a5b7c1361374a7b069f59d435
unknown <ngakanputu39@gmail.com>
2022-01-13 17:59:32 +0800

    add menu

--- bangun-datar-calculator/main.c
+++ bangun-datar-calculator/main.c
@@ -2,8 +2,37 @@
 #include <stdlib.h>
 
 /* Header Liblary */
+#include "resources/menu.c"
 
 int main()
 {
     int status=0;
+    int pilihan;
+    while(!status)
+    {
+        /* Calling procedure "menu.c" */
+        menu();
+        
+        scanf("\n%d",&pilihan);
+        getchar();
+
+        switch(pilihan)
+        {
+            case 1:
+                printf("\n Berhasil \n");
+                getchar();
+                break;
+            case 2:
+                segitiga();
+                getchar();
+                break;
+            case 3:
+                return 0;
+                break;
+            default:
+                printf("\n Tidak Berhasil \n");
+                getchar();
+                break;
+        }
+    }
 }
---
+++ bangun-datar-calculator/resources/menu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu()
+{   
+    system("cls");
+    printf("\n [1] Persegi      ");
+    printf("\n [2] Segitiga     ");
+    printf("\n [3] Exit         ");
+    printf("\n# Pilihan : ");
+}
